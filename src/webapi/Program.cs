d1a6ceef12d67555bffd78233fdfb2d3774a4bfe
prenaissance <alex.andries.aa@gmail.com>
2024-09-10 21:54:09 +0300

    feat(webapi): add new grpc method & implement it

--- src/webapi/Program.cs
+++ src/webapi/Program.cs
@@ -19,11 +19,14 @@ builder.Services.AddSingleton(mongoDatabase);
 builder.Services.Configure<MongoDbConfiguration>(builder.Configuration);
 builder.Services.Configure<BackpackTfConfiguration>(builder.Configuration);
 builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<PricingService>();
+app.MapGrpcReflectionService();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
