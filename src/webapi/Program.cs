878935c863225c18537f7c2fe4b428c62b3e05a2
prenaissance <alex.andries.aa@gmail.com>
2024-10-12 00:21:33 +0300

    fix(webapi): use etcd client instead of script

--- src/webapi/Program.cs
+++ src/webapi/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using dotnet_etcd;
+using Grpc.Core;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Conventions;
@@ -36,4 +39,50 @@ app.MapGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 app.MapGet("/healthz/liveness", () => "OK");
 
+string GetHostname()
+{
+    var host = builder.Configuration.GetValue<string>("HOST");
+    if (!string.IsNullOrEmpty(host))
+    {
+        return host;
+    }
+    var hostname = builder.Configuration.GetValue<string>("HOSTNAME");
+    if (!string.IsNullOrEmpty(hostname))
+    {
+        return hostname;
+    }
+
+    return Environment.MachineName;
+}
+
+// service registration
+async Task RegisterService()
+{
+    var etcdHosts = builder.Configuration.GetValue<string>("ETCD_HOSTS");
+    if (string.IsNullOrEmpty(etcdHosts))
+    {
+        Console.WriteLine("ETCD_HOSTS is not set, skipping service registration");
+        return;
+    }
+
+    var etcdClient = new EtcdClient(etcdHosts, configureChannelOptions: options =>
+    {
+        options.Credentials = ChannelCredentials.Insecure;
+    });
+    await etcdClient.PutAsync($"/services/pricing-tf/{GetHostname()}", JsonSerializer.Serialize(
+        new
+        {
+            host = GetHostname(),
+            healthCheck = new
+            {
+                port = 8080,
+                path = "/healthz/liveness",
+                delaySeconds = 10
+            }
+        }
+    ));
+    Console.WriteLine($"Service registered at /services/pricing-tf/{GetHostname()}");
+}
+
+await RegisterService();
 app.Run();
