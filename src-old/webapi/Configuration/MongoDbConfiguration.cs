54816836fd8befa27e49c08955bf6485d1cc84d6
prenaissance <95138356+prenaissance@users.noreply.github.com>
2025-12-13 21:13:52 +0200

    chore: remove remnants of the F# project

--- src-old/webapi/Configuration/MongoDbConfiguration.cs
+++
@@ -1,6 +0,0 @@
-namespace PricingTf.WebApi.Configuration;
-
-public record MongoDbConfiguration(
-    string MongoDbUrl = "mongodb://localhost:27117",
-    string MongoDbName = "backpack-tf-replica"
-);
